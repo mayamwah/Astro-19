1e119a8efc59fea3c47f119d5cd11ca7329bd908
mayamwah <mmileswa@ucsc.edu>
2024-10-01 11:02:52 -0700

    added helloworld.py
    now this is repo is less boring

---
+++ hello-world.py
@@ -0,0 +1,19 @@
+#this program will write
+#Hello World!
+
+#This function tells the
+#computer to print
+#"Hello World
+def PrintHelloWorld():
+	print("Hello World!")
+
+#This defines our main()
+#function for our program
+def main():
+	PrintHelloWorld()
+
+	#When we run the program
+	#thisexecutesfirst
+if __name__=="__main__":
+	main()
+
